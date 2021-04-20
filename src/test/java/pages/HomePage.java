7452db945779caf1829a231207e9e36beb72d55b
guvan <guvanch.nasyrov@gmail.com>
2021-04-19 20:43:17 -0500

    Test case 4 and test case 5

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -34,7 +34,6 @@ public class HomePage {
     public WebElement placeOrderButton;
     @FindBy(id = "reset")
     public WebElement resetButton;
-    //
     @FindBy(id = "dialog")
     public WebElement successMessage;
 
